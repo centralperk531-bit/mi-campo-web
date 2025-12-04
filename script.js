8215d6008b40c253403b683029b2230e35606f85
centralperk531-bit <centralperk.531@gmail.com>
2025-12-04 12:21:57 +0100

    Fix typo in CONFIG description field

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby_llWLzIYpMB
 const CONFIG = {
     nombreCampo: "Casa Paraíso",
     descripcion: "Tu escapada perfecta",
-    descripción 2: "Licencia <strong>VTAR/CA/03260<strong>"
+    descripción: "VTAR/CA/03260>"
     capacidad: "15 personas",
     precioPorNoche: 200,
     estanciaMinima: 2,
